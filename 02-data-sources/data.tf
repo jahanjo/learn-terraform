3dd0e4e19a5b5f270182a16838fcfe854f92b0f6
Jahangeer <jahangeerkallagunta9@gmail.com>
2023-06-15 21:35:00 +0100

    data-sources

--- 02-data-sources/data.tf
+++ 02-data-sources/data.tf
@@ -9,3 +9,13 @@ output "ami" {
 
   value = data.aws_ami.example
 }
+
+data "aws_instance" "foo" {
+  instance_id = "i-052838d8aafcdba8e"
+
+
+}
+
+output "instance" {
+  value = "data.aws_instance.foo.public_ip"
+}
