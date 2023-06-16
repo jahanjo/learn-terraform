2474e96bb47247ab5dd6f02ee03f603b735ab279
Jahangeer <jahangeerkallagunta9@gmail.com>
2023-06-16 13:22:44 +0100

    Variable Data types List Variable type and Map Variable type

--- 03-vars/vars.tf
+++ 03-vars/vars.tf
@@ -6,14 +6,16 @@ output "sample" {
   value = var.sample
 }
 
+#String data types
 variable "sample1" {
-  default = Hello_World
+  default = "Hello_World"
 }
 
 output "sample1" {
   value = var.sample1
 }
 
+#Numeric data types
 variable "sample2" {
   default = 100
 }
@@ -22,10 +24,42 @@ output "sample2" {
   value = var.sample2
 }
 
+#Boolean Data types
 variable "sample3" {
   default = true
 }
 
 output "sample3" {
   value = var.sample3
+}
+
+#Defaulault variable type
+variable "sample4" {
+  default = 100
+}
+
+#List Variable type
+variable "sample6" {
+  default = [
+    100,
+    "abc",
+    true
+  ]
+}
+
+output "sample6" {
+  value = var.sample6[1]
+}
+
+#Map variable type
+variable "sample7" {
+  default = {
+    number = 100
+    string = "abc"
+    boolean = true
+  }
+}
+
+output "sample7" {
+  value = var.sample7["number"]
 }
