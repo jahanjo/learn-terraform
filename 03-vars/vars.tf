45f3e9568ac828963af1f887e9deda01122d3303
Jahangeer <jahangeerkallagunta9@gmail.com>
2023-06-16 13:09:41 +0100

    Variable Data types

--- 03-vars/vars.tf
+++ 03-vars/vars.tf
@@ -10,10 +10,22 @@ variable "sample1" {
   default = "Hello_World"
 }
 
+output "sample1" {
+  value = var.sample1
+}
+
 variable "sample2" {
   default = 100
 }
 
+output "sample2" {
+  value = var.sample2
+}
+
 variable "sample3" {
   default = true
+}
+
+output "sample3" {
+  value = var.sample3
 }
