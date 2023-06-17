87938406ba217e35ccd2727fa38deca67194da4a
Jahangeer <jahangeerkallagunta9@gmail.com>
2023-06-17 21:22:59 +0100

    Variable Data types List Variable type and Map Variable type SHEll variable Provider and Resources State file storage Security group

---
+++ 06-modules/modules.tf
@@ -0,0 +1,19 @@
+module "sg" {
+  source = "./sg"
+}
+
+module "ec2" {
+  source = "./ec2"
+  security_group_id = module.sg.security_group_id
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-jahan"
+    key    = "05-remote-state.tfstate"
+    region = "us-east-1"
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+}
