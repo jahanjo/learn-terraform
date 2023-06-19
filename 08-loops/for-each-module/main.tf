3f7e0fae5683734c6b8f34e53a1b489ab0d5c4e9
Jahangeer <jahangeerkallagunta9@gmail.com>
2023-06-19 14:15:11 +0100

    Variable Data types List Variable type and Map Variable type SHEll variable Provider and Resources State file storage Security group modules Functions Count count_Maps for_each

---
+++ 08-loops/for-each-module/main.tf
@@ -0,0 +1,20 @@
+variable "components" {
+  default = {
+    cart = {
+      name = "cart",
+      instance_type = "t3.small"
+    }
+    catalogue = {
+      name = "catalogue",
+      instance_type = "t3.micro"
+    }
+  }
+}
+
+module "ec2" {
+  source = "./module"
+
+  for_each = var.components
+  instance_type = each.value.instance_type
+  name = each.value.name
+}
